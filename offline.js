85c8fa9f0fa0b2d62ef2d5b53da34f5654214a8b
andvagorion <andva@gmx.net>
2017-11-08 12:56:26 +0100

    test

--- offline.js
+++ offline.js
@@ -3,7 +3,7 @@
 
   if ('serviceWorker' in navigator) {
     console.log('ServiceWorker registration in progress.');
-    navigator.serviceWorker.register('service-worker.js').then(function() {
+    navigator.serviceWorker.register('worker.js').then(function() {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
       console.log('ServiceWorker registration failed: ', err);
